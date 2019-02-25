7ccdefc125b6a2a3ad7621325b667e7c430d10bf
José Pinto <jose.t.pinto@tecnico.ulisboa.pt>
2019-02-25 23:53:02 +0000

    Removing API KEY

--- php/newsletter-subscribe.php
+++ php/newsletter-subscribe.php
@@ -10,7 +10,7 @@ include('./mailchimp/mailchimp.php');
 use \DrewM\MailChimp\MailChimp;
 
 // Step 1 - Set the apiKey - How get your Mailchimp API KEY - http://kb.mailchimp.com/article/where-can-i-find-my-api-key
-$apiKey 	= 'f3b2f264a632829a8cafecf85c154437-us20';
+$apiKey 	= '-';
 
 // Step 2 - Set the listId - How to get your Mailchimp LIST ID - http://kb.mailchimp.com/article/how-can-i-find-my-list-id
 $listId 	= 'testing';
